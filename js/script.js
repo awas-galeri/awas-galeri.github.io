02593cc6c9c0d9029dabd60a0e7dbcbb53249490
Awas Galeri <awasgaleri@gmail.com>
2022-12-23 11:40:33 +0700

    create first project

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Parallax
+// About
+$(window).on('load', function() {
+    $('.pKiri').addClass('pMuncul');
+    $('.pKanan').addClass('pMuncul');
+})
+
+
+// Parallax
+$(window).scroll(function () {
+
+    // Jumbotron
+    let wScroll = $(this).scrollTop();
+
+    $('.jumbotron h1').css({
+        'transform': 'translate(0px, ' + wScroll / 9 + '%)'
+    });
+
+
+    // Portfolio
+    if (wScroll > $('#portfolio').offset().top - 250) {
+        $('#portfolio .card').each(function (i) {
+            setTimeout(function () {
+                $('#portfolio .card').eq(i).addClass('muncul');
+            }, 400 * (i+1));
+        });
+    }
+});
