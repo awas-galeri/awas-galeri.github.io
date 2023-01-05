e7fef19bb71aa1a23c555bfbc8226bcdbe4a0fdd
Awas Galeri <awasgaleri@gmail.com>
2023-01-05 08:41:23 +0700

    edit parallax about

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 // Parallax
 // About
-$(window).on('load', function() {
-    $('.pKiri').addClass('pMuncul');
-    $('.pKanan').addClass('pMuncul');
-})
+// $(window).on('load', function () {
+//     $('.pKiri').addClass('pMuncul');
+//     $('.pKanan').addClass('pMuncul');
+// })
 
 
 // Parallax
@@ -16,13 +16,19 @@ $(window).scroll(function () {
         'transform': 'translate(0px, ' + wScroll / 9 + '%)'
     });
 
+    // About
+    if (wScroll > $('#about').offset().top - 250) {
+        $('.pKiri').addClass('pMuncul');
+        $('.pKanan').addClass('pMuncul');
+    }
+
 
     // Portfolio
     if (wScroll > $('#portfolio').offset().top - 250) {
         $('#portfolio .card').each(function (i) {
             setTimeout(function () {
                 $('#portfolio .card').eq(i).addClass('muncul');
-            }, 400 * (i+1));
+            }, 400 * (i + 1));
         });
     }
 });
